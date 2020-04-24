b1af92eb74bf96e180e82f9a988689ce524cdd97
Charlie Kim <cwkim3@gmail.com>
2020-04-24 19:51:07 -0400

    added project screenshots and error-handling in Contacts

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import styles from './Projects.module.css';
-import Context from '../../Context/Context';
 import ProjectCard from '../../Components/ProjectCard/ProjectCard';
+import Context from '../../Context/Context';
+import styles from './Projects.module.css';
 
 export default class Projects extends React.Component {
   static contextType = Context;
@@ -17,28 +17,28 @@ export default class Projects extends React.Component {
       },
       {
         name: 'PERFECT DATE',
-        img: 'rhcp-quiz-app.png',
+        img: 'perfect-date-app.png',
         tech: ['html5', 'css', 'javascript', 'jquery'],
         info:
           'The Perfect Date App allows you to plan, and keep track of all the logistics and details that go into the perfect date by utilizing multiple APIs (Seat Geek API, Google GeoCode API, Zomato API) to help users search and find local entertainment and restaurants within walking distance.',
       },
       {
         name: 'METROCLEANERS',
-        img: 'rhcp-quiz-app.png',
+        img: 'metrocleaners-app.png',
         tech: ['react', 'nodejs', 'express', 'css'],
         info:
           'MetroCleaners App is a customer relationship management app for local drycleaning businesses that keeps track of customers and their orders while using automated SMS notifications via Twilio API to keep them in the loop.',
       },
       {
         name: 'POST-UP',
-        img: 'rhcp-quiz-app.png',
+        img: 'post-up-app.png',
         tech: ['react', 'nodejs', 'express', 'css'],
         info:
           'Post-Up App allows you to create, organize and find pickup basketball games in your area. Each game has a roster-list of players who will be attending and also a commentboard to allow players to communicate and discuss anything they want before the game.',
       },
       {
         name: 'STRYD',
-        img: 'rhcp-quiz-app.png',
+        img: 'stryd-app.png',
         tech: ['angular', 'ionic', 'sass', 'css'],
         info: 'STRYD is a venue-management app that allows venues to manage their promoters, street-teams, and ticket sales.',
       },
